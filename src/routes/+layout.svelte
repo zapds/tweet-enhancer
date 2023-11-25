5704f1e0e12db9ba9106ecf7adc78cac4a82165f
Zapd0s <71247896+Zapd0s@users.noreply.github.com>
2023-11-25 19:41:07 +0530

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import "../app.css";
 </script>
 
+<div class="flex flex-row items-center bg-black border-b border-b-gray-600 p-4">
+  <img src="/favicon.png" alt="nav-icon" height="32px" width="32px">
+  <div class="mx-4">
+    <a href="/" class="text-2xl font-mono text-white pr-16">Tweet Enhancer</a>
+    <a href="/about" class="button text-white">About</a>
+  </div>
+</div>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-8xl text-center">
-    Hello world!
+<h1 class="text-8xl text-center text-white">
+    Hello, World
 </h1>
